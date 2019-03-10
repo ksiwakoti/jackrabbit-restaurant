9923ffeffa29a1eb2b063544365534b45874ddcb
ksiwakoti <40340359+ksiwakoti@users.noreply.github.com>
2019-03-10 15:42:56 -0500

    Add files via upload

--- client/src/components/aboutusCom/aboutus.js
+++ client/src/components/aboutusCom/aboutus.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react';
 class AboutusCom extends Component {
   render() {
     return (
-      <div className="AboutusMainDiv">
-        //TODO: Aboutus Same For Everyone
+      <div className='AboutUSContainer'>
+        <div className='leftDivCon'>
+          <div className='missionCon'>
+          </div>
+          <div className='adressphCn'>
+          </div>
+          <div className='socialMedia'>
+          </div>
+        </div>
+        <div className='rightDivCon'>
+          <div className='Missionstatementtextcon'></div>
+          <div className='ContactUScon'></div>
+          <div className='logosCon'></div>
+            <div className='leftDiv'>
+              <div className='facebookcon'></div>
+              <div className='InstaCon'></div>
+            </div>
+            <div className='rightDiv'>
+              <div className='twitterdiv'></div>
+              <div className='snapChatDiv'></div>
+            </div>
+        </div>
       </div>
     );
   }
